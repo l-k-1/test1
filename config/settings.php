f939f76a9285ddb0ce6a8d8ab2749caedc3680c3
Alexey Kolesnik <lyolik1984@ukr.net>
2018-05-13 14:02:56 +0200

    add config/ app, settings

---
+++ config/settings.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'slider_path' => 'slider-cycle',
+    'home_portfolio_count' => 5,
+    'home_articles_count' => 3,
+    'pagination' => 2,
+    'recent_comments' => 3,
+    'recent_portfolios' => 3,
+
+
+];
