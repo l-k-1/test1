2790724ba199407d175a4a4666908fdb4b7d3877
Alexey Kolesnik <lyolik1984@ukr.net>
2018-05-13 14:08:55 +0200

    route web

---
+++ routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+    /*Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Auth::routes();
+
+    Route::get('/home', 'HomeController@index')->name('home');*/
+
+
+Route::resource('/','IndexController',[
+                                    'only' => ['index'],
+                                    'names' => [
+                                        'index' => 'home',
+                                    ],
+]);
+
+Route::resource('portfolios','PortfoliosController',[
+                                                    'parameters' => [
+                                                        'portfolios' => 'alias',
+                                                    ],
+
+]);
+
+Route::resource('articles','ArticlesController',[
+                                                'parameters' => [
+                                                    'articles' => 'alias',
+                                                ],
+
+]);
+
+Route::get('aricles/cat/{cat_alias?}',['uses'=>'CategoryController@index','as'=>'articlesCat']);
