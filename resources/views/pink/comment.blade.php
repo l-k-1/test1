a5722548a5db6bb7eb840916060e460ff6f87e18
Alexey Kolesnik <lyolik1984@ukr.net>
2018-05-19 20:10:11 +0200

    add comment (part 3, lesson 18)

--- resources/views/pink/comment.blade.php
+++ resources/views/pink/comment.blade.php
@@ -3,7 +3,7 @@
         <div id="comment-{{ $item->id }}" class="comment-container">
             <div class="comment-author vcard">
                 @set($hash, isset($item->email) ? md5($item->email) : md5($item->user->email))
-                <img alt="" src="https://www.gravatar.com/avatar/{{$hash}}?d==mm&s=75" class="avatar" height="75" width="75" />
+                <img alt="" src="https://www.gravatar.com/avatar/{{$hash}}?d=mm&s=75" class="avatar" height="75" width="75" />
                 <cite class="fn">{{ $item->user->name or $item->name}}</cite>
             </div>
             <!-- .comment-author .vcard -->
@@ -26,5 +26,11 @@
             <!-- .comment-meta .commentmetadata -->
         </div>
         <!-- #comment-##  -->
+
+        @if(isset($com[$item->id]))
+            <ul class="children">
+                @include (env('THEME').'.comment',['items'=>$com[$item->id]])
+            </ul>
+        @endif
     </li>
 @endforeach
